d85923683a10795b6a1bcc39797230e7eced9101
LeviDuarte <142362891+LeviDuarte@users.noreply.github.com>
2025-01-10 23:16:12 +0000

    new practices in php file

---
+++ php_practice/algorithms/bubblesort.php
@@ -0,0 +1,15 @@
+<?php
+ 
+function bubbleSort (array $arr):array{
+ $swapped;
+ $n = count($arr);
+//  echo "the length of the $arr is $n" ;
+ for ($i = 0; $i >= $n -1; $i++){
+    $swapped = false;
+    for (let )
+    echo "$arr[$i], "; 
+ }
+  return $arr;
+};
+$arr = [1, 2, 3, 4];
+bubbleSort($arr);
